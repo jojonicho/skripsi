5d1d9d5aa16b8cbf8037212bc93f1d8a5d9d9944
Jonathan Nicholas <jojonicho181@gmail.com>
2023-03-23 17:02:50 +0700

    update test-shared

--- tests/test-shared.sh
+++ tests/test-shared.sh
@@ -1,3 +1,4 @@
+export CLIENT_ZONE_ASIA="$(kubectl get pods | grep -o 'ta-client-deployment-\w*-\w*\b')"
 for i in {1..10}
 do
   kubectl exec -it $CLIENT_ZONE_ASIA -c ta-client -- sh -c 'curl http://ta-server-service.sharedvpc.svc.clusterset.local:8000/todos'
