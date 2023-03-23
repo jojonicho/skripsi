65b67bd46228e468ee922d12a8bf2db899dc7291
Jonathan Nicholas <jojonicho181@gmail.com>
2023-03-23 18:20:01 +0700

    update asm vegeta script

--- tests/asm-vegeta.sh
+++ tests/asm-vegeta.sh
@@ -10,7 +10,7 @@ do
   # attack
   
   kubectl run vegeta --attach --restart=Never --image="peterevans/vegeta" -- sh -c \
-    "echo 'GET http://ta-server-service:8080/todos' | vegeta attack -rate=$RPS -duration=5s -output=ha.bin && cat ha.bin" > ${OUTPUT_DIR}/results.${RPS}rps.bin
+    "echo 'GET http://ta-server-service.sharedvpc:8080/todos' | vegeta attack -rate=$RPS -duration=5s -output=ha.bin && cat ha.bin" > ${OUTPUT_DIR}/results.${RPS}rps.bin
 
   vegeta report -type=text ${OUTPUT_DIR}/results.${RPS}rps.bin
   kubectl delete pod vegeta
