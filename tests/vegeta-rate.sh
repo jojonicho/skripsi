f62c7038fca5d37bef7f27f08b485457bec83cd6
Jonathan Nicholas <jojonicho181@gmail.com>
2023-03-24 18:51:29 +0700

    add vegeta rate

--- tests/vegeta-rate.sh
+++ tests/vegeta-rate.sh
@@ -7,7 +7,7 @@ sleep 20
 # attack
 
 kubectl run vegeta --attach --restart=Never --image="peterevans/vegeta" -- sh -c \
-  "echo 'GET http://ta-server-service.sharedvpc:8080/todos' | vegeta attack -rate=infinity -duration=5s -output=ha.bin && cat ha.bin" > ${OUTPUT_DIR}/results.rps.bin
+  "echo 'GET http://$VIP_FLEET/todos' | vegeta attack -rate=infinity -duration=5s -output=ha.bin && cat ha.bin" > ${OUTPUT_DIR}/results.rps.bin
 
 vegeta report -type=text ${OUTPUT_DIR}/results.rps.bin
 kubectl delete pod vegeta
