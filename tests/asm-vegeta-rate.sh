a94c73309713c37a5813d258ed5e7c4302167df2
Jonathan Nicholas <jojonicho181@gmail.com>
2023-03-24 17:04:07 +0700

    add max rate vegeta script

--- tests/asm-vegeta-rate.sh
+++ tests/asm-vegeta-rate.sh
@@ -8,8 +8,8 @@ sleep 20
 # attack
 
 kubectl run vegeta --attach --restart=Never --image="peterevans/vegeta" -- sh -c \
-  "echo 'GET http://ta-server-service.sharedvpc:8080/todos' | vegeta attack -rate=0 -duration=5s -output=ha.bin && cat ha.bin" > ${OUTPUT_DIR}/results.${RPS}rps.bin
+  "echo 'GET http://ta-server-service.sharedvpc:8080/todos' | vegeta attack -rate=0 -duration=5s -output=ha.bin && cat ha.bin" > ${OUTPUT_DIR}/results.rps.bin
 
-vegeta report -type=text ${OUTPUT_DIR}/results.${RPS}rps.bin
+vegeta report -type=text ${OUTPUT_DIR}/results.rps.bin
 kubectl delete pod vegeta
 
