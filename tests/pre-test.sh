2b39d2e22baaa0c8a5908cf1d70c1b9422541428
Jonathan Nicholas <jojonichoo@gmail.com>
2023-03-24 19:22:40 +0700

    add multicluster test results

--- tests/pre-test.sh
+++ tests/pre-test.sh
@@ -2,4 +2,4 @@ export CLIENT_ZONE_ASIA="$(kubectl get pods | grep -o 'ta-client-deployment-\w*-
 
 export VIP_FLEET="$(kubectl describe mci ta-server-ingress | grep -w 'VIP:' | tail -1 | sed 's/VIP:[[:space:]]*//; s/^[[:space:]]*//')"
 
-kubectl exec -it $CLIENT_ZONE_ASIA -c ta-client -- sh -c "curl -s $VIP_FLEET/todos"
+kubectl exec -it $CLIENT_ZONE_ASIA -c ta-client -- sh -c "curl -s http://$VIP_FLEET/todos"
