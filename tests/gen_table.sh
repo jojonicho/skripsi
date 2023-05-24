c96d38c083da5996d809afc2b0e636f97388b56f
Jonathan Nicholas <jojonicho181@gmail.com>
2023-05-25 02:07:23 +0700

    refactor table gen script

--- tests/gen_table.sh
+++ tests/gen_table.sh
@@ -10,17 +10,17 @@ OUTPUT_DIRS=($MC_SEA $MC_AUS $ASM_SEA $ASM_AUS)
 
 
 # REMOVE THIS BLOCK AFTER vegeta report is integrated while testing
-for OUTPUT_DIR in "${OUTPUT_DIRS[@]}"
-do
-  for RPS in "${RPS_LIST[@]}"
-  do
-    #vegeta report -type=json $OUTPUT_DIR/results.${RPS}rps.bin > $OUTPUT_DIR/${RPS}.json
-    for i in {1..3}
-      do
-        echo $OUTPUT_DIR-$i/results.${RPS}rps.bin
-        vegeta report -type=json $OUTPUT_DIR-$i/results.${RPS}rps.bin > $OUTPUT_DIR-$i/${RPS}.json
-      done
-  done
-done
+#for OUTPUT_DIR in "${OUTPUT_DIRS[@]}"
+#do
+  #for RPS in "${RPS_LIST[@]}"
+  #do
+    ##vegeta report -type=json $OUTPUT_DIR/results.${RPS}rps.bin > $OUTPUT_DIR/${RPS}.json
+    #for i in {1..3}
+      #do
+        #echo $OUTPUT_DIR-$i/results.${RPS}rps.bin
+        #vegeta report -type=json $OUTPUT_DIR-$i/results.${RPS}rps.bin > $OUTPUT_DIR-$i/${RPS}.json
+      #done
+  #done
+#done
 
 python3.8 table.py $MC_SEA $MC_AUS $ASM_SEA $ASM_AUS
