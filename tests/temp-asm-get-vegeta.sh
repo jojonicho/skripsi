f0009dbbaa8941f97242fb8c2ff81147c7cf3141
Jonathan Nicholas <jojonicho181@gmail.com>
2023-05-24 20:49:57 +0700

    vg

--- tests/temp-asm-get-vegeta.sh
+++ tests/temp-asm-get-vegeta.sh
@@ -9,9 +9,6 @@ for RPS in "${RPS_LIST[@]}"
 do
   kubectl exec -it $CLIENT_ZONE_ASIA -c ta-vegeta -- "echo 'GET http://ta-server-service.sharedvpc:8080/todos' | vegeta attack -rate=$RPS -duration=5s -output=ha.bin && cat ha.bin" > ${OUTPUT_DIR}/results.${RPS}rps.bin
 
-  kubectl run vegeta --attach --restart=Never --image="peterevans/vegeta" -- sh -c \
-    "echo 'GET http://ta-server-service.sharedvpc:8080/todos' | vegeta attack -rate=$RPS -duration=5s -output=ha.bin && cat ha.bin" > ${OUTPUT_DIR}/results.${RPS}rps.bin
-
   #vegeta report -type=text ${OUTPUT_DIR}/results.${RPS}rps.bin
   #vegeta report -type=json $OUTPUT_DIR/results.${RPS}rps.bin > $OUTPUT_DIR/${RPS}.json
 done
