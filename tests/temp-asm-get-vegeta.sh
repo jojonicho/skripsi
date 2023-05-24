ef7b865e783d4a4b8753f2bec0861fda12be007b
Jonathan Nicholas <jojonicho181@gmail.com>
2023-05-24 22:02:21 +0700

    fix asm vegeta

--- tests/temp-asm-get-vegeta.sh
+++ tests/temp-asm-get-vegeta.sh
@@ -6,6 +6,7 @@ mkdir $OUTPUT_DIR
 
 for RPS in "${RPS_LIST[@]}"
 do
+  sleep 20
   kubectl exec -it $CLIENT_ZONE_ASIA -c ta-vegeta -- sh -c \ "echo 'GET http://ta-server-service.sharedvpc:8080/todos' | vegeta attack -rate=$RPS -duration=5s -output=ha.bin && cat ha.bin" > ${OUTPUT_DIR}/results.${RPS}rps.bin
 
   vegeta report -type=text ${OUTPUT_DIR}/results.${RPS}rps.bin
