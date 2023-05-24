9b642352ddbf071fc0c364a4324aa646a8e92a26
Jonathan Nicholas <jojonicho181@gmail.com>
2023-05-24 22:46:41 +0700

    fix vegeta script

--- tests/temp-asm-get-vegeta.sh
+++ tests/temp-asm-get-vegeta.sh
@@ -7,8 +7,9 @@ mkdir $OUTPUT_DIR
 for RPS in "${RPS_LIST[@]}"
 do
   sleep 20
-  kubectl exec -it $CLIENT_ZONE_ASIA -c ta-vegeta -- sh -c \ "echo 'GET http://ta-server-service.sharedvpc:8080/todos' | vegeta attack -rate=$RPS -duration=5s -output=ha.bin && cat ha.bin" > ${OUTPUT_DIR}/results.${RPS}rps.bin
-
+  #kubectl exec -it $CLIENT_ZONE_ASIA -c ta-vegeta -- sh -c \ "echo 'GET http://ta-server-service.sharedvpc:8080/todos' | vegeta attack -rate=$RPS -duration=5s -output=ha.bin && cat ha.bin" > ${OUTPUT_DIR}/results.${RPS}rps.bin
+  kubectl exec -it $CLIENT_ZONE_ASIA -c ta-vegeta -- sh -c \ "echo 'GET http://ta-server-service.sharedvpc:8080/todos' | vegeta attack -rate=$RPS -duration=5s -output=ha.bin"
+  kubectl exec -it $CLIENT_ZONE_ASIA -c ta-vegeta -- cat ha.bin" > ${OUTPUT_DIR}/results.${RPS}rps.bin
   vegeta report -type=text ${OUTPUT_DIR}/results.${RPS}rps.bin
   vegeta report -type=json $OUTPUT_DIR/results.${RPS}rps.bin > $OUTPUT_DIR/${RPS}.json
 done
