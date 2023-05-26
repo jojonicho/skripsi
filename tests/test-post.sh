d3513a195fb87e1dfc33b49b5dac8837abfcd1b1
Jonathan Nicholas <jojonicho181@gmail.com>
2023-05-26 15:39:52 +0700

    update mcs mci test scripts

--- tests/test-post.sh
+++ tests/test-post.sh
@@ -1,9 +1,5 @@
 export CLIENT_ZONE_ASIA="$(kubectl get pods | grep -o 'ta-client-deployment-\w*-\w*\b')"
-
-#kubectl exec -it $CLIENT_ZONE_ASIA -c ta-client -- sh -c "curl -s $VIP_FLEET/todos"
-
-#kubectl exec -it $CLIENT_ZONE_ASIA -c ta-client -- curl -X POST "$VIP_FLEET/todos" -H 'Content-Type: application/json' -d '{"title": "AUSTRALIA"}'
+export VIP_FLEET="$(kubectl describe mci ta-server-ingress | grep -w 'VIP:' | tail -1 | sed 's/VIP:[[:space:]]*//; s/^[[:space:]]*//')"
 
 kubectl exec -it $CLIENT_ZONE_ASIA -c ta-client -- curl -X POST $VIP_FLEET/todos -H 'Content-Type: application/json' -d "{\"title\": \"$CLUSTER_NAME\"}"
 
-#kubectl exec -it $CLIENT_ZONE_ASIA -c ta-client -- curl -X POST $VIP_FLEET/todos -H 'Content-Type: application/json' -d '{"title": "'"$CLUSTER_NAME"'"}'
